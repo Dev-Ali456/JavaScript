f28925c6ebc24977495340eae31ade74abfc0a9b
Syed Ali Abrar <105586082+Dev-Ali456@users.noreply.github.com>
2025-06-25 12:53:21 -0700

    Update celsiusToFahrenheit.js

--- JS Week 1 /celsiusToFahrenheit.js
+++ JS Week 1 /celsiusToFahrenheit.js
@@ -1,8 +1,8 @@
 // Declare a variable for temperature in Celsius
-let celsius = 25; // You can change this value
+let celsius = 25;
 
 // Convert Celsius to Fahrenheit
 var fahrenheit = (celsius * 9/5) + 32;
 
-// Display the result
+// lets Display the result
 console.log(celsius + "°C is equal to " + fahrenheit + "°F");
