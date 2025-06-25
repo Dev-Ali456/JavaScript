2d153fd3194a606bb483612413cddd655396a770
Syed Ali Abrar <105586082+Dev-Ali456@users.noreply.github.com>
2025-06-25 12:54:39 -0700

    Update simple-interest.js

--- JS Week 1 /simple-interest.js
+++ JS Week 1 /simple-interest.js
@@ -1,10 +1,10 @@
 // Declare variables for principal, rate, and time
-let principal = 1000; // You can change this value
+let principal = 1000; 
 let rate = 5; // Annual interest rate in percentage
 let time = 3; // Time in years
 
 // Calculate simple interest
 var simpleInterest = (principal * rate * time) / 100;
 
-// Display the result
+// lets Display the result
 console.log("The simple interest is: " + simpleInterest);
