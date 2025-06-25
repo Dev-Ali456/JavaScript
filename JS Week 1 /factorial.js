3f790845bbdaef6640ffeddf5320019aea344469
Syed Ali Abrar <105586082+Dev-Ali456@users.noreply.github.com>
2025-06-25 13:30:56 -0700

    Calculate Factorial of a Number
    This script calculates the factorial of a given number using a for loop. It initializes a factorial variable and multiplies it sequentially from 1 up to the number. The result is then displayed in the console.

---
+++ JS Week 1 /factorial.js
@@ -0,0 +1,11 @@
+// Declare a variable for the number
+let number = 5;
+var factorial = 1;
+
+// Calculate factorial
+for (let i = 1; i <= number; i++) {
+    factorial *= i;
+}
+
+// Lets Display the result
+console.log("The factorial of " + number + " is " + factorial);
