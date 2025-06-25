c8e3280fbcb41fa68f9e388739c418a33f56a297
Syed Ali Abrar <105586082+Dev-Ali456@users.noreply.github.com>
2025-06-25 12:13:43 -0700

    Basic JS Variables Var_declaredFor_width_and_height
    This is a simple JavaScript program that demonstrates how to declare variables using let and var. It defines width and height to calculate the area of a rectangle and outputs the result using console.log.

---
+++ Basic JSVariables/Var_declaredFor_width_and_height.js
@@ -0,0 +1,8 @@
+// Declare variables for width and height
+let width = 5;  // You can change this value
+let height = 10; // You can change this value
+// Calculate the area of the rectangle
+var area = width * height;
+// Display the result
+console.log("The area of the rectangle is: " + area);
+
