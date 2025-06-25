51f08c37f04c285ea873b017391226bf51726d74
Syed Ali Abrar <105586082+Dev-Ali456@users.noreply.github.com>
2025-06-25 11:49:45 -0700

    Update letVar.js

--- Basic JS/Variables/letVar.js
+++ Basic JS/Variables/letVar.js
@@ -33,7 +33,8 @@ let hello = 'Hello world!';
 let message;
 // copy 'Hello world' from hello into message
 message = hello;
-
+console.log(hello);
 // now two variables hold the same data
-alert(hello); // Hello world!
-alert(message); // Hello world!
+// alert(hello); // Hello world!
+// alert(message); // Hello world!
+
